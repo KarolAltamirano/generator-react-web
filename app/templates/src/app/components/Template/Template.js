df98d09335a3de1ee7caac2bbabb3038d7d9e4e3
Karol Altamirano <karol.altamirano@unit9.com>
2016-07-05 17:40:21 +0200

    Add velocity example

--- app/templates/src/app/components/Template/Template.js
+++ app/templates/src/app/components/Template/Template.js
@@ -4,11 +4,39 @@ import { bindActionCreators } from 'redux';
 import AppActions from '../../actions/AppActions';
 import style from './template.scss';
 
+import 'velocity-animate';
+import 'velocity-animate/velocity.ui';
+import { VelocityComponent, velocityHelpers } from 'velocity-react';
+
 /**
  * Template React Component
  */
 class Template extends React.Component {
     render() {
+        var animationProps = {
+            duration: 400,
+            easing: 'spring',
+            // delay: 1500,
+            begin: () => console.log('animation begin'),
+            progress: () => console.log('animation progress'),
+            complete: () => console.log('animation complete'),
+            animation: {
+                translateX: this.props.template * 10
+            }
+        };
+
+        var animationPropsTwo = {
+            duration: 400,
+            // delay: 0,
+            animation: velocityHelpers.registerEffect({
+                defaultDuration: 1000,
+                calls: [
+                    [{ translateX: this.props.template * 10 }, 0.5, { easing: 'linear' }],
+                    [{ rotateZ: this.props.template * 90 }, 0.5, { easing: 'spring' }]
+                ]
+            })
+        };
+
         return (
             <div>
                 <button
@@ -24,6 +52,16 @@ class Template extends React.Component {
                     Hello React (Async)
                 </button>
                 <span className={style.template}>{this.props.template}</span>
+                <div className={style.container}>
+                    <VelocityComponent {...animationProps}>
+                        <div className={style.element}></div>
+                    </VelocityComponent>
+                </div>
+                <div className={style.container}>
+                    <VelocityComponent {...animationPropsTwo}>
+                        <div className={style.element}></div>
+                    </VelocityComponent>
+                </div>
             </div>
         );
     }
