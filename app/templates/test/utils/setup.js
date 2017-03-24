6559d18b7186e422c195002dff112f6198b83e42
Karol Altamirano <kaltamirano@noon.com>
2017-03-24 20:37:38 +0400

    Add flow to unit tests

--- app/templates/test/utils/setup.js
+++ app/templates/test/utils/setup.js
@@ -1,20 +1,22 @@
+// @flow
+
 import { jsdom } from 'jsdom';
 
-function noop() { return null; }
+const noop = () => 0;
 
-require.extensions['.css'] = noop;
-require.extensions['.scss'] = noop;
-require.extensions['.svg'] = noop;
-require.extensions['.png'] = noop;
-require.extensions['.jpg'] = noop;
-require.extensions['.gif'] = noop;
+(require: any).extensions['.css'] = noop;
+(require: any).extensions['.scss'] = noop;
+(require: any).extensions['.svg'] = noop;
+(require: any).extensions['.png'] = noop;
+(require: any).extensions['.jpg'] = noop;
+(require: any).extensions['.gif'] = noop;
 
 const exposedProperties = ['window', 'document'];
 
 global.document = jsdom('<!doctype html><html><body></body></html>');
 global.window = global.document.defaultView;
 
-Object.keys(global.window).forEach((property) => {
+Object.keys(global.window).forEach((property: string) => {
     if (typeof global[property] === 'undefined') {
         exposedProperties.push(property);
         global[property] = global.window[property];
