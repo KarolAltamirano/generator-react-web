da6aa0c39ae4127d2f60e9862f6ecb3cd071ace0
Karol Altamirano <kaltamirano@noon.com>
2017-03-25 02:06:36 +0400

    Update indentation

--- app/index.js
+++ app/index.js
@@ -11,52 +11,52 @@ const yosay = require('yosay');
 const _ = require('lodash');
 
 module.exports = class extends Generator {
-    prompting() {
-        this.log(yosay(`Welcome to the ${chalk.green('React Web')} Generator!`));
+  prompting() {
+    this.log(yosay(`Welcome to the ${chalk.green('React Web')} Generator!`));
 
-        const prompts = [
-            {
-                type: 'input',
-                name: 'name',
-                message: 'Your project name (use kebab-case)',
-                default: _.kebabCase(this.appname)
-            }
-        ];
+    const prompts = [
+      {
+        type: 'input',
+        name: 'name',
+        message: 'Your project name (use kebab-case)',
+        default: _.kebabCase(this.appname)
+      }
+    ];
 
-        return this.prompt(prompts).then((props) => {
-            this.props = props;
-        });
+    return this.prompt(prompts).then((props) => {
+      this.props = props;
+    });
+  }
+
+  writing() {
+    // copy all files except .gitignore and package.json
+    this.fs.copy(
+      this.templatePath('**/!(.gitignore|.npmignore|package.json)'),
+      this.destinationPath(), { globOptions: { dot: true } }
+    );
+
+    // copy .gitignore
+    if (this.fs.exists(this.templatePath('.npmignore'))) {
+      this.fs.copy(
+        this.templatePath('.npmignore'),
+        this.destinationPath('.gitignore')
+      );
+    } else {
+      this.fs.copy(
+        this.templatePath('.gitignore'),
+        this.destinationPath('.gitignore')
+      );
     }
 
-    writing() {
-        // copy all files except .gitignore and package.json
-        this.fs.copy(
-            this.templatePath('**/!(.gitignore|.npmignore|package.json)'),
-            this.destinationPath(), { globOptions: { dot: true } }
-        );
+    // copy package.json
+    this.fs.copyTpl(
+      this.templatePath('package.json'),
+      this.destinationPath('package.json'),
+      { name: _.kebabCase(this.props.name) }
+    );
+  }
 
-        // copy .gitignore
-        if (this.fs.exists(this.templatePath('.npmignore'))) {
-            this.fs.copy(
-                this.templatePath('.npmignore'),
-                this.destinationPath('.gitignore')
-            );
-        } else {
-            this.fs.copy(
-                this.templatePath('.gitignore'),
-                this.destinationPath('.gitignore')
-            );
-        }
-
-        // copy package.json
-        this.fs.copyTpl(
-            this.templatePath('package.json'),
-            this.destinationPath('package.json'),
-            { name: _.kebabCase(this.props.name) }
-        );
-    }
-
-    install() {
-        this.runInstall('yarn');
-    }
+  install() {
+    this.runInstall('yarn');
+  }
 };
--- tools/start.js
+++ tools/start.js
@@ -3,25 +3,25 @@ import { js_beautify as beautify } from 'js-beautify'; // eslint-disable-line ca
 import pkg from '../app/templates/package.json';
 
 function build() {
-    return new Promise((resolve, reject) => {
-        pkg.name = '<%= name %>';
+  return new Promise((resolve, reject) => {
+    pkg.name = '<%= name %>';
 
-        // eslint-disable-next-line camelcase
-        const content = beautify(JSON.stringify(pkg), { indent_size: 2, end_with_newline: true });
+    // eslint-disable-next-line camelcase
+    const content = beautify(JSON.stringify(pkg), { indent_size: 2, end_with_newline: true });
 
-        fs.writeFile('app/templates/package.json', content, (err) => {
-            if (err) {
-                reject(err);
-                return;
-            }
+    fs.writeFile('app/templates/package.json', content, (err) => {
+      if (err) {
+        reject(err);
+        return;
+      }
 
-            resolve();
-        });
+      resolve();
     });
+  });
 }
 
 console.log('Starting \'build\' ...');
 
 build()
-    .then(() => console.log('Finished \'build\''))
-    .catch(err => console.error(err));
+  .then(() => console.log('Finished \'build\''))
+  .catch(err => console.error(err));
