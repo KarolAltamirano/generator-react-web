3cfeaf4860802f7b190d7b24036114d7c47c2134
Karol Altamirano <kaltamirano@noon.com>
2017-04-09 23:12:42 +0400

    Configure e2e tests

--- app/templates/teste2e/example.spec.e2e.js
+++ app/templates/teste2e/example.spec.e2e.js
@@ -1,16 +1,51 @@
 // @flow
 
-/* eslint func-names: 0 */
+import nightwatch from 'nightwatch';
+import config from '../nightwatch.json';
 
-/*
-module.exports = {
-  'Demo test': function (browser) {
+describe('Page', () => {
+  const client = nightwatch.initClient(config);
+  const browser = client.api();
+
+  after((done: Function) => {
+    browser.end();
+    client.start(done);
+  });
+
+  it('should have title "Project Title"', (done: Function) => {
     browser
+      .useCss()
       .url(browser.launchUrl)
-      .waitForElementVisible('.loader', 3000)
-      .waitForElementNotVisible('.loader', 3000)
-      .waitForElementVisible('#container', 3000)
-      .end();
-  }
-};
-*/
+      .waitForElementVisible('body', 5000)
+      .assert.title('Project Title');
+
+    client.start(done);
+  });
+
+  it('should have counter set to 1', (done: Function) => {
+    const xPathElement = '//*[@id="app"]/div/div[1]/span';
+
+    browser
+      .useXpath()
+      .url(browser.launchUrl)
+      .waitForElementPresent(xPathElement, 5000)
+      .assert.containsText(xPathElement, '1');
+
+    client.start(done);
+  });
+
+  it('should have counter set to 3', (done: Function) => {
+    const xPathButton = '//*[@id="app"]/div/div[1]/button[1]';
+    const xPathElement = '//*[@id="app"]/div/div[1]/span';
+
+    browser
+      .useXpath()
+      .url(browser.launchUrl)
+      .waitForElementPresent(xPathButton, 5000)
+      .waitForElementPresent(xPathElement, 5000)
+      .click(xPathButton)
+      .assert.containsText(xPathElement, '3');
+
+    client.start(done);
+  });
+});
